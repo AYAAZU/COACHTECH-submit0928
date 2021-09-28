b8e215bcdb8e28783eabb5e2f91e32af5f6ba3d4
ユーザー名AYAAZU <azusasuza1020@yahoo.co.jp>
2021-09-28 17:36:42 +0900

    firstcommit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+const menu = document.getElementById("menu")
+
+menu.addEventListener("click",()=>{
+  menu.classList.toggle("open")
+  const navbp = document.getElementById("navbp")
+  navbp.classList.toggle("open")
+}
+)
